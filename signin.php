061f8cbc1f43fa4d9151117fa5f979c7a47195af
Alex-Mth <96028055+Alex-Mth@users.noreply.github.com>
2023-10-30 12:27:49 +0530

    prodeal upload complete

---
+++ signin.php
@@ -0,0 +1,118 @@
+<?php
+include 'config.php';
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Check if form was submitted
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    // SQL query to check if the user exists with the given email and password
+    $sql = "SELECT * FROM user WHERE email='$email' AND password='$password'";
+
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // User found, get their id and redirect accordingly
+        $row = $result->fetch_assoc();
+        $userId = $row['userid'];
+
+        if ($userId == 0) {
+            // Admin
+            header("Location: admin.php");
+            exit();
+        } elseif ($userId == 1) {
+            // Regular user
+            session_start(); // Start the session
+            $_SESSION['username'] = $row['username']; // Store the username in the session
+            header("Location: index.php");
+            exit();
+        } elseif ($userId == 2) {
+            // Seller
+            session_start(); // Start the session
+            $_SESSION['username'] = $row['username']; // Store the username in the session
+            header("Location: seller.php");
+            exit();
+        } else {
+            // Invalid user id
+            echo "Invalid user ID.";
+        }
+    } else {
+        // User not found, display an error message
+        echo "User does not exist.";
+    }
+}
+
+$conn->close();
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sign Up</title>
+
+    <!-- Font Icon -->
+    <link rel="stylesheet" href="fonts/material-icon/css/material-design-iconic-font.min.css">
+
+    <!-- Main css -->
+    <link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+
+    <div class="main">
+
+<!-- Sign in  Form -->
+<section class="sign-in">
+    <div class="container">
+        <div class="signin-content">
+            <div class="signin-image">
+                <figure><img src="img/780.png" ></figure>   
+                <a href="signup.php" class="signup-image-link">Create an account</a>
+            </div>
+
+            <div class="signin-form">
+                <h2 class="form-title">Sign In</h2>
+                <form method="POST" class="register-form" id="login-form">
+                    <div class="form-group">
+                        <label for="email"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                        <input type="text" name="email" id="email" placeholder="Your Email"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="password"><i class="zmdi zmdi-lock"></i></label>
+                        <input type="password" name="password" id="password" placeholder="Password"/>
+                    </div>
+                    <div class="form-group">
+                        <input type="checkbox" name="remember-me" id="remember-me" class="agree-term" />
+                        <label for="remember-me" class="label-agree-term"><span><span></span></span>Remember me</label>
+                    </div>
+                    <div class="form-group form-button">
+                        <input type="submit" name="signin" id="signin" class="form-submit" value="Log in"/>
+                    </div>
+                </form>
+                <div class="social-login">
+                    <span class="social-label">Or login with</span>
+                    <ul class="socials">
+                        <li><a href="#"><i class="display-flex-center zmdi zmdi-facebook"></i></a></li>
+                        <li><a href="#"><i class="display-flex-center zmdi zmdi-twitter"></i></a></li>
+                        <li><a href="#"><i class="display-flex-center zmdi zmdi-google"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+</div>
+<!-- JS -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="js/login.js"></script>
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,107 @@
+<?php
+include 'config.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Check if form was submitted
+    $uname = $_POST['uname'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $password = $_POST['pass'];
+    $confirm_password = $_POST['re_pass'];
+
+    // Debugging: Output form data
+    //echo "Name: " . $name . "<br>";
+    //echo "Email: " . $email . "<br>";
+    //echo "Phone: " . $phone . "<br>";
+    //echo "Password: " . $password . "<br>";
+    //echo "Confirm Password: " . $confirm_password . "<br>";
+
+    // Validate form data (check for empty fields)
+    if (empty($uname)||empty($name) || empty($email) || empty($phone) || empty($password) || empty($confirm_password)) {
+        echo "All fields are required.";
+    } else {
+        $sql = "INSERT INTO user (username,name, email, phone, password) VALUES ('$uname','$name', '$email', '$phone', '$password')";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "Record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    }
+}
+$conn->close();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sign Up Form by Colorlib</title>
+
+    <!-- Font Icon -->
+    <link rel="stylesheet" href="fonts/material-icon/css/material-design-iconic-font.min.css">
+
+    <!-- Main css -->
+    <link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+
+    <div class="main">
+
+        <!-- Sign up form -->
+        <section class="signup">
+            <div class="container">
+                <div class="signup-content">
+                    <div class="signup-form">
+                        <h2 class="form-title">Sign up</h2>
+                        <form method="POST" class="register-form" id="register-form">
+    <div class="form-group">
+        <label for="uname"><i class="zmdi zmdi-account material-icons-name"></i></label>
+        <input type="text" name="uname" id="uname" placeholder="Create a username" required />
+    </div>
+    <div class="form-group">
+        <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
+        <input type="text" name="name" id="name" placeholder="Your Full Name" required />
+    </div>
+    <div class="form-group">
+        <label for="email"><i class="zmdi zmdi-email"></i></label>
+        <input type="email" name="email" id="email" placeholder="Your Email" required />
+    </div>
+    <div class="form-group">
+        <label for="phone"><i class="zmdi zmdi-phone"></i></label>
+        <input type="number" name="phone" id="phone" placeholder="Your Phone number" required />
+    </div>
+    <div class="form-group">
+        <label for="pass"><i class="zmdi zmdi-lock"></i></label>
+        <input type="password" name="pass" id="pass" placeholder="Password" required />
+    </div>
+    <div class="form-group">
+        <label for="re-pass"><i class="zmdi zmdi-lock-outline"></i></label>
+        <input type="password" name="re_pass" id="re_pass" placeholder="Repeat your password" required />
+    </div>
+    <div class="form-group">
+        <input type="checkbox" name="agree-term" id="agree-term" class="agree-term" required />
+        <label for="agree-term" class="label-agree-term"><span><span></span></span>I agree all statements in <a href="#" class="term-service">Terms of service</a></label>
+    </div>
+    <div class="form-group form-button">
+        <input type="submit" name="signup" id="signup" class="form-submit" value="Register" />
+    </div>
+</form>
+
+                    </div>
+                    <div class="signup-image">
+                        <figure><img src="img/signup-image.jpg" alt="sing up image"></figure>
+                        <a href="signin.php" class="signup-image-link">I am already member</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    <!-- JS -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="js/login.js"></script>
+</body>
+</html>

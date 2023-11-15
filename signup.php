e0f002a0ba55c3b2fa83ab0085cef4d8acb561ec
Abhijith13055 <diablomorpheus1@gmail.com>
2023-11-15 18:21:19 +0530

    updated the signup page

--- signup.php
+++ signup.php
@@ -9,46 +9,94 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $phone = $_POST['phone'];
     $password = $_POST['pass'];
     $confirm_password = $_POST['re_pass'];
-    $address=$_POST['address'];
-
-    // Debugging: Output form data
-    //echo "Name: " . $name . "<br>";
-    //echo "Email: " . $email . "<br>";
-    //echo "Phone: " . $phone . "<br>";
-    //echo "Password: " . $password . "<br>";
-    //echo "Confirm Password: " . $confirm_password . "<br>";
+    $address = $_POST['address'];
 
     // Validate form data (check for empty fields)
-    if (empty($uname)||empty($name) || empty($email) || empty($phone) || empty($password) || empty($confirm_password) ||  empty($address))  {
+    if (empty($uname) || empty($name) || empty($email) || empty($phone) || empty($password) || empty($confirm_password) || empty($address)) {
         echo "All fields are required.";
     } else {
-        $sql = "INSERT INTO user (username,name, email, phone, password,address) VALUES ('$uname','$name', '$email', '$phone', '$password','$address')";
+        $sql = "INSERT INTO user (username, name, email, phone, password, address) VALUES ('$uname', '$name', '$email', '$phone', '$password', '$address')";
 
         if ($conn->query($sql) === TRUE) {
-            echo "Record created successfully";
+            // Redirect to the same page to avoid form resubmission
+            header("Location: {$_SERVER['PHP_SELF']}");
+            exit();
         } else {
             echo "Error: " . $sql . "<br>" . $conn->error;
         }
     }
 }
+
 $conn->close();
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Sign Up Form by Colorlib</title>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Sign Up Form by Colorlib</title>
+    
+        <!-- Font Icon -->
+        <link rel="stylesheet" href="fonts/material-icon/css/material-design-iconic-font.min.css">
+    
+        <!-- Main css -->
+        <link rel="stylesheet" href="css/login.css">
+<style>
+        /* Modal Styles */
+.modal {
+  display: block;
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgb(0, 0, 0);
+  background-color: rgba(0, 0, 0, 0.4);
+  padding-top: 60px;
+}
 
-    <!-- Font Icon -->
-    <link rel="stylesheet" href="fonts/material-icon/css/material-design-iconic-font.min.css">
+.modal-content {
+  background-color: #fefefe;
+  margin: 10% auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
+  text-align: center;
+}
 
-    <!-- Main css -->
-    <link rel="stylesheet" href="css/login.css">
-</head>
-<body>
+.close {
+  color: #aaaaaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+  cursor: pointer;
+}
+
+.close:hover,
+.close:focus {
+  color: #000;
+  text-decoration: none;
+}
+
+#modalOkBtn {
+  background-color: #4caf50;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+</style>
+    </head>
+    <body>
 
     <div class="main">
 
@@ -110,5 +158,57 @@ $conn->close();
     <!-- JS -->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="js/login.js"></script>
+
+    <!-- Modal Box -->
+<div id="myModal" class="modal">
+  <!-- Modal content -->
+  <div class="modal-content">
+    <span class="close" onclick="closeModal()">&times;</span>
+    <p>Registration successful! Click OK to close.</p>
+    <button id="modalOkBtn" onclick="closeModal()">OK</button>
+  </div>
+</div>
+
+<script>
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var signUpButton = document.getElementById('signup');
+
+// When the user clicks the button, open the modal
+signUpButton.onclick = function() {
+  modal.style.display = 'block';
+}
+
+// Function to close the modal and optionally redirect to signin.php
+function closeModal(isOkButton) {
+  modal.style.display = 'none';
+  if (isOkButton) {
+    window.location.href = 'signin.php';
+  }
+}
+
+// When the user clicks outside the modal, close it
+window.onclick = function(event) {
+  if (event.target === modal) {
+    closeModal(false);
+  }
+}
+
+// Event listener for the close button (&times;)
+document.getElementsByClassName('close')[0].onclick = function() {
+  closeModal(false);
+};
+
+// Event listener for the OK button
+document.getElementById('modalOkBtn').onclick = function() {
+  closeModal(true);
+};
+</script>
+
+
+
+
 </body>
 </html>
